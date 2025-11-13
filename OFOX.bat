67bc89902a596033e21547c58187784a2e78f9ca
Wildan Hermawan <hermawanwildan78@gmail.com>
2025-11-13 13:37:56 +0700

    Update OFOX.bat

--- OFOX.bat
+++ OFOX.bat
@@ -2,4 +2,11 @@
 title OrangeFox Recovery Installer for Xiaomi Redmi Note 5A (UGGLITE)
 color 0B
 echo. 
-echo  ======
+echo  ==============================================================
+echo   OrangeFox Recovery Installer for Xiaomi Redmi Note 5A (UGGLITE)
+echo  --------------------------------------------------------------
+echo   Created by : Wildan Hermawan
+echo   Github     : https://github.com/ran4yyou
+echo   Description: Flash and boot OrangeFox recovery automatically
+echo  ==============================================================
+ECHO.
